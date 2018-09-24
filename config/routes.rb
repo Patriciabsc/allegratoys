ce98e4e51dc6093194e9ac9a8b82919d54f15ef2
Patriciabsc <bernasconi.patricia@gmail.com>
2018-09-24 12:10:21 +0200

    creating controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, only [:index, :show]
+  resources :products, only: [:index, :show]
 
-  resources :categories, only [:index, :show]
+  resources :categories, only: [:index, :show]
   resources :categories do
-    resources :products, only [:index, :show]
+    resources :products, only: [:index, :show]
   end
 
 
